870c641cf6366ca33add105b82d443394633ede6
Toki Migimatsu <toki.migimatsu@gmail.com>
2022-07-23 22:35:52 -0700

    Remove old files

--- toki/pose.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-import numpy as np
-
-
-@dataclass
-class Pose:
-    position: np.ndarray = np.zeros(3)
-    quaternion: np.ndarray = np.array([0.0, 0.0, 0.0, 1.0])
